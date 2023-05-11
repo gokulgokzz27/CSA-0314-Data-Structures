c5af8cdff6ad257d53f46a9c528fd55383c5626a
gokulgokzz27 <113032757+gokulgokzz27@users.noreply.github.com>
2023-05-11 08:40:26 +0530

    Create 4 Natural numbers in .c

---
+++ 4 Natural numbers in .c
@@ -0,0 +1,31 @@
+#include<stdio.n>
+
+#include<conio.n>
+
+main()
+
+{
+
+int n;
+
+int i;
+
+clrscr();
+
+printf("enter the value of n")
+
+scanf("%d",&n);
+
+for(i=1; i<n; i++)
+
+{
+
+printf("%d",\n&i);
+
+}
+
+getch();
+
+return o;
+
+}
